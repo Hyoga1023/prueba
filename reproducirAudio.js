a718f440b1b004e8d49ce7bb9f1f63965684fb3c
Cesar Martinez <cesarmartinez99@gmail.com>
2025-06-23 12:00:03 -0500

    Acomoda botón pagina1 y agrega mp3

---
+++ reproducirAudio.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const audio = new Audio('./mp3/Ha_llegado_un_angel.mp3');
+
+  audio.autoplay = true;
+
+  // Control de volumen (opcional)
+  audio.volume = 0.2; // Rango: 0.0 a 1.0
+
+  audio.play().catch(error => {
+    console.warn("Reproducción bloqueada por el navegador:", error);
+  });
+});
